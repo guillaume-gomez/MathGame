b27eb8e00f813bd0abb8a2b591e420f200da95f3
guillaume-gomez <guillaume.gomez846@gmail.com>
2017-10-23 22:11:27 +0200

    add method to check if a button is focused

--- include/ButtonPerso.hpp
+++ include/ButtonPerso.hpp
@@ -40,6 +40,7 @@ class ButtonPerso :  public sf::Sprite
         virtual ~ButtonPerso();
         void handle_input(sf::Event& event, sf::RenderTarget& target);
         void setAlpha (int _alpha);
+        bool isFocused() const;
         void draw(sf::RenderTarget& app);
         bool isClicked() const;
         void unclick();
@@ -47,6 +48,7 @@ class ButtonPerso :  public sf::Sprite
     protected:
         std::string m_filename;
         bool m_clicked;
+        bool m_isFocused;
         sf::Texture m_texture;
 };
 
@@ -60,6 +62,8 @@ inline void ButtonPerso::setAlpha (int _alpha)
 {
     this->setColor(sf::Color(255, 255, 255, _alpha));
 }
+
+inline bool ButtonPerso::isFocused() const { return m_isFocused;}
 
 inline void ButtonPerso::unclick()
 {
