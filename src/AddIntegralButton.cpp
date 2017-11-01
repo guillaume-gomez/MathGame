a1e0c7f485d9e5a3c84f476d9aefdbb2502cbe0d
guillaume-gomez <guillaume.gomez846@gmail.com>
2017-11-02 00:13:27 +0100

    improve the class by adding some methods

--- src/AddIntegralButton.cpp
+++ src/AddIntegralButton.cpp
@@ -3,39 +3,45 @@
 AddIntegralButton::AddIntegralButton(const char* _filename)
 : ButtonPerso(_filename)
 {
+    m_window = sfg::Window::Create();
+    m_window->SetTitle("Test");
     m_layout = sfg::Box::Create(sfg::Box::Orientation::HORIZONTAL);
-	auto scale = sfg::Scale::Create(sfg::Scale::Orientation::HORIZONTAL);
-	auto scrollbar = sfg::Scrollbar::Create(sfg::Scrollbar::Orientation::VERTICAL);
+    const char* items[] = {"-25", "-24", "-23", "-22", "-21", "-20", "-19", "-18", "-17", "-16", "-15", "-14", "-13", "-12", "-11", "-10", "-9", "-8", "-7", "-6", "-5", "-4", "-3", "-2", "-1", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25"};
+    m_min_combo_box = sfg::ComboBox::Create();
+    m_max_combo_box = sfg::ComboBox::Create();
+    m_min_combo_box->SetRequisition(sf::Vector2f(20.f, 10.f));
+    m_max_combo_box->SetRequisition(sf::Vector2f(20.f, 10.f));
+    for(auto i = 0; i < 51; ++i)
+    {
+        m_min_combo_box->AppendItem(items[i]);
+        m_max_combo_box->AppendItem(items[i]);
+    }
+    m_button = sfg::Button::Create("Add");
+    m_layout->Pack(m_min_combo_box);
+    m_layout->Pack(m_max_combo_box);
+	m_layout->Pack(m_button);
+    m_layout->SetSpacing(10.f);
 
-	m_adjustment = scrollbar->GetAdjustment();
-	scale->SetAdjustment(m_adjustment);
-
-	// Tune the adjustment parameters.
-	m_adjustment->SetLower(20.f);
-	m_adjustment->SetUpper(100.f);
-
-	m_adjustment->SetMinorStep(3.f);
-	m_adjustment->SetMajorStep(12.f);
-
-	m_adjustment->SetPageSize(20.f);
-
-	//m_adjustment->GetSignal( sfg::Adjustment::OnChange ).Connect( std::bind( &RangeExample::AdjustmentChange, this ) );
-
-	scale->SetRequisition(sf::Vector2f(80.f, 20.f));
-	scrollbar->SetRequisition(sf::Vector2f(0.f, 80.f));
-
-	auto scalebox = sfg::Box::Create( sfg::Box::Orientation::VERTICAL );
-	scalebox->Pack(scale, false, false);
-
-	m_layout->Pack(scalebox);
-	m_layout->Pack(scrollbar);
-	// Set box spacing
-	m_layout->SetSpacing(5.f);
+    m_window->Add(m_layout);
+    m_window->SetPosition(sf::Vector2f(300.0f, 300.0f));
+    hide();
 }
 
-void AddIntegralButton::addToWindow(sfg::Window::Ptr window)
+void AddIntegralButton::update()
 {
-    window->Add(m_layout);
+    m_window->Update(0.0f);
+}
+
+
+void AddIntegralButton::hide()
+{
+    m_window->Show(false);
+}
+
+void AddIntegralButton::handle_input(sf::Event& event, sf::RenderTarget& target)
+{
+    //handle_input(event, target);
+    m_window->HandleEvent(event);
 }
 
 AddIntegralButton::~AddIntegralButton()
